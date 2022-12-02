fae7d4a52d88166a26460fddcef0224b522b1d02
SwitchCaseKam <kamil.leczycki.ns@gmail.com>
2022-12-03 00:02:13 +0100

    use setInterval to couting process, minor styles updates

--- script.js
+++ script.js
@@ -1,117 +1,86 @@
-const timer = ms => new Promise(res => {
-  console.log('ppp')
-  return setTimeout(res, ms)
-})
-
 const goButton = document.getElementById('go-button');
-const againButton = document.getElementById('again-button');
-
 const downLights = document.getElementsByClassName('light-down');
+const primaryTimeText = document.getElementById('primary-time-text');
+const secondaryTimeText = document.getElementById('secondary-time-text');
 
-const timeTextParagraph = document.getElementById('time-text');
+let countingInProgress = false;
+let reactionMeasurementDone = false
 
 let startTime = 0;
 let endTime = 0;
 
-let countingInProgress = false;
-
-
 let countingSound = new Audio('audio/start-13691.mp3');
 let startSound = new Audio('audio/usp-pistol-sfx-80490.mp3');
 
-let intervalID;
-
+let lightsCountingIntervalCallback;
+const timer = ms => new Promise(res => setTimeout(res, ms));
 
 goButton.addEventListener('click', () => {
   if (!countingInProgress) {
-    testSetInterval();
+    startLightsInterval();
   } else {
-    clearInterval(intervalID);
-    turnOffAllLights();
-    testSetInterval();
+    stopCounter();
+    if (reactionMeasurementDone) {
+      clearInterval(lightsCountingIntervalCallback);
+      turnOffAllLights();
+    }
   }
-});
-
-againButton.addEventListener('click', () => {
-  startCounter();
 });
 
 document.addEventListener('keydown', (event) => {
   if (event.code === 'Space') {
     if (!countingInProgress) {
-      startCounter();
+      startLightsInterval();
     } else {
+      stopCounter();
       if (reactionMeasurementDone) {
-        startCounter();
-      } else {
-        stopCounter();
+        clearInterval(lightsCountingIntervalCallback);
+        turnOffAllLights();
       }
     }
   }
 });
 
-async function startCounter() {
-  reactionMeasurementDone = false;
-  startTime = 0;
-  endTime = 0;
-  timeTextParagraph.innerHTML = '';
-  againButton.style.visibility = 'hidden';
-  goButton.style.visibility = 'visible';
-  countingInProgress = true;
-  for (let i=0 ; i< downLights.length; i++) {
-    downLights[i].style.backgroundColor = 'red';
-    countingSound.play();
-    await timer(1000);
-    
-    if (i === downLights.length-1) {
-      for (let j=0 ; j< downLights.length; j++) {
-        downLights[j].style.backgroundColor = '#484643';
-        startSound.play();
-        startTime = Date.now();
-        setTimeout(()=> {
-          againButton.style.visibility = 'visible';
-          countingInProgress = false;
-        }, 1500);
-      }
-    }
-  }  
-}
-
 function stopCounter() {
   endTime = Date.now();
   if (startTime === 0 || endTime === 0) {
-    timeTextParagraph.innerHTML = 'Falstart! Too early!';
+    primaryTimeText.innerHTML = 'Falstart! Too early!';
   } else {
     const timeDiff = endTime - startTime;
     console.log('[LOG] startTime: ', startTime, '; endTime: ', endTime, '; timeDiff: ', timeDiff);
-    timeTextParagraph.innerHTML = 'Your reaction time: ' + timeDiff/1000 + ' s';
+    primaryTimeText.innerHTML = 'Your reaction time: ' + timeDiff/1000 + ' s';
   }
-  goButton.style.visibility = 'hidden';
+  secondaryTimeText.innerHTML = 'Tap to try again';
+  goButton.style.visibility = 'visible';
+  reactionMeasurementDone = true;
   countingInProgress = false;
 }
 
-
-function testSetInterval() {
-  let i = 0;
-  intervalID = setInterval(() =>{
-    console.log('i = ', i);
+function startLightsInterval() {
+  reactionMeasurementDone = false;
+  primaryTimeText.innerHTML = '';
+  secondaryTimeText.innerHTML = '';
+  goButton.style.visibility = 'visible';
+  startTime = 0;
+  endTime = 0;
+  let lightIndex = 0;
+  lightsCountingIntervalCallback = setInterval(() =>{
     countingInProgress = true;
-    if (i === downLights.length) {
-      clearInterval(intervalID);
-      countingInProgress = false;
+    if (lightIndex === downLights.length) {
+      clearInterval(lightsCountingIntervalCallback);
       turnOffAllLights();
       startSound.play();
+      startTime = Date.now();
     } else {
       countingSound.play();
-      downLights[i].style.backgroundColor = 'red';
+      downLights[lightIndex].style.backgroundColor = 'red';
     }
-    i++;
+    lightIndex++;
   }, 1000);
 }
 
 function turnOffAllLights() {
   for (let j=0 ; j< downLights.length; j++) {
-    console.log('a')
     downLights[j].style.backgroundColor = '#484643';
   }
 }
